215faacd53ac3ef1823e99412667cce1df24324e
Servando <vandotorresg@gmail.com>
2024-08-16 13:28:38 +0200

    change potery

--- content_agent/src/content_agent/main.py
+++ content_agent/src/content_agent/main.py
@@ -21,42 +21,5 @@ def run():
     ContentAgentCrew().crew().kickoff(inputs=inputs)
 
 
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "Self Improvement"
-    }
-    try:
-        ContentAgentCrew().crew().train(n_iterations=int(sys.argv[1]), inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        ContentAgentCrew().crew().replay(task_id=sys.argv[0])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
 if __name__ == "__main__":
-    # Check if an argument is provided
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "train":
-            if len(sys.argv) > 2:
-                train()
-            else:
-                print("Please provide the number of iterations for training.")
-        elif sys.argv[1] == "replay":
-            if len(sys.argv) > 2:
-                replay()
-            else:
-                print("Please provide the task ID for replay.")
-    else:
-        # If no arguments, run the default run() function
-        run()
+    run()
